851a3bdb019ec4013e9745db204a2172ff30f441
Maki10 <nemanjammaric@gmail.com>
2018-07-23 17:35:16 +0200

    Portfolio fix

---
+++ application/models/Portfolio.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(dirname(__FILE__).'/'.'Query_Builder.php'); 
+
+class Model_Portfolio extends Query_Builder
+{
+	
+}
---
+++ application/models/Team.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(dirname(__FILE__).'/'.'Query_Builder.php'); 
+
+class Model_Team extends Query_Builder
+{
+	
+}
